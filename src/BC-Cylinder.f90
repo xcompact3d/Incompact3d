c04a39355af667b2e2c0d57f7eb44fff47b61ce2
Paul Bartholomew <ptb0890@gmail.com>
2019-02-19 20:37:00 +0000

    Formatting

--- src/BC-Cylinder.f90
+++ src/BC-Cylinder.f90
@@ -16,15 +16,15 @@ module cyl
   USE param
 
   IMPLICIT NONE
-  
+
   integer :: FS
   character(len=100) :: fileformat
   character(len=1),parameter :: NL=char(10) !new line character
-  
+
   !probes
   integer, save :: nprobes, ntimes1, ntimes2
   integer, save, allocatable, dimension(:) :: rankprobes, nxprobes, nyprobes, nzprobes
-  
+
   real(mytype),save,allocatable,dimension(:) :: usum,vsum,wsum,uusum,uvsum,uwsum,vvsum,vwsum,wwsum
 
   PRIVATE ! All functions/subroutines private by default
@@ -33,13 +33,13 @@ module cyl
 contains
 
   subroutine geomcomplex_cyl(epsi,nxi,nxf,ny,nyi,nyf,nzi,nzf,dx,yp,dz,remp)
-    
+
     use decomp_2d, only : mytype
     use param, only : zero, one, two
     use ibm
 
     implicit none
-   
+
     real(mytype),dimension(nxi:nxf,nyi:nyf,nzi:nzf) :: epsi
     real(mytype),dimension(ny) :: yp
     integer                    :: nxi,nxf,ny,nyi,nyf,nzi,nzf
@@ -54,7 +54,7 @@ contains
        zeromach = zeromach/two
     end do
     zeromach = 1.0e1*zeromach
-    
+
     do k=nzi,nzf
        do j=nyi,nyf
           ym=yp(j)
@@ -68,213 +68,213 @@ contains
           enddo
        enddo
     enddo
-  
+
     return
   end subroutine geomcomplex_cyl
 
-!********************************************************************
-subroutine boundary_conditions_cyl (ux,uy,uz,phi)
+  !********************************************************************
+  subroutine boundary_conditions_cyl (ux,uy,uz,phi)
 
-  USE param
-  USE variables
-  USE decomp_2d
+    USE param
+    USE variables
+    USE decomp_2d
 
-  implicit none
+    implicit none
 
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux,uy,uz,ep1
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux,uy,uz,ep1
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi
 
-  call inflow (ux,uy,uz,phi)
-  call outflow (ux,uy,uz,phi)
+    call inflow (ux,uy,uz,phi)
+    call outflow (ux,uy,uz,phi)
 
-  return
-end subroutine boundary_conditions_cyl
-!********************************************************************
-subroutine inflow (ux,uy,uz,phi)
+    return
+  end subroutine boundary_conditions_cyl
+  !********************************************************************
+  subroutine inflow (ux,uy,uz,phi)
 
-  USE param
-  USE variables
-  USE decomp_2d
+    USE param
+    USE variables
+    USE decomp_2d
 
-  implicit none
+    implicit none
 
-  integer  :: j,k,is
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux,uy,uz
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi
+    integer  :: j,k,is
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux,uy,uz
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi
 
-  call random_number(bxo)
-  call random_number(byo)
-  call random_number(bzo)
-  do k=1,xsize(3)
-     do j=1,xsize(2)
-        bxx1(j,k)=one+bxo(j,k)*inflow_noise
-        bxy1(j,k)=zero+byo(j,k)*inflow_noise
-        bxz1(j,k)=zero+bzo(j,k)*inflow_noise
-     enddo
-  enddo
+    call random_number(bxo)
+    call random_number(byo)
+    call random_number(bzo)
+    do k=1,xsize(3)
+       do j=1,xsize(2)
+          bxx1(j,k)=one+bxo(j,k)*inflow_noise
+          bxy1(j,k)=zero+byo(j,k)*inflow_noise
+          bxz1(j,k)=zero+bzo(j,k)*inflow_noise
+       enddo
+    enddo
 
-  if (iscalar.eq.1) then
-     do is=1, numscalar
-        do k=1,xsize(3)
-           do j=1,xsize(2)
-              phi(1,j,k,is)=cp(is)
-           enddo
-        enddo
-     enddo
-  endif
+    if (iscalar.eq.1) then
+       do is=1, numscalar
+          do k=1,xsize(3)
+             do j=1,xsize(2)
+                phi(1,j,k,is)=cp(is)
+             enddo
+          enddo
+       enddo
+    endif
 
-  return
-end subroutine inflow
-!********************************************************************
-subroutine outflow (ux,uy,uz,phi)
+    return
+  end subroutine inflow
+  !********************************************************************
+  subroutine outflow (ux,uy,uz,phi)
 
-  USE param
-  USE variables
-  USE decomp_2d
-  USE MPI
+    USE param
+    USE variables
+    USE decomp_2d
+    USE MPI
 
-  implicit none
+    implicit none
 
-  integer :: i,j,k,is,code
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux,uy,uz
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi
-  real(mytype) :: udx,udy,udz,uddx,uddy,uddz,cx,cz,uxmin,uxmax,uxmin1,uxmax1
+    integer :: i,j,k,is,code
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux,uy,uz
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi
+    real(mytype) :: udx,udy,udz,uddx,uddy,uddz,cx,cz,uxmin,uxmax,uxmin1,uxmax1
 
-  udx=one/dx; udy=one/dy; udz=one/dz; uddx=half/dx; uddy=half/dy; uddz=half/dz
+    udx=one/dx; udy=one/dy; udz=one/dz; uddx=half/dx; uddy=half/dy; uddz=half/dz
 
-  uxmax=-1609.
-  uxmin=1609.
-  do k=1,xsize(3)
-     do j=1,xsize(2)
-        if (ux(nx-1,j,k).gt.uxmax) uxmax=ux(nx-1,j,k)
-        if (ux(nx-1,j,k).lt.uxmin) uxmin=ux(nx-1,j,k)
-     enddo
-  enddo
+    uxmax=-1609.
+    uxmin=1609.
+    do k=1,xsize(3)
+       do j=1,xsize(2)
+          if (ux(nx-1,j,k).gt.uxmax) uxmax=ux(nx-1,j,k)
+          if (ux(nx-1,j,k).lt.uxmin) uxmin=ux(nx-1,j,k)
+       enddo
+    enddo
 
-  call MPI_ALLREDUCE(uxmax,uxmax1,1,real_type,MPI_MAX,MPI_COMM_WORLD,code)
-  call MPI_ALLREDUCE(uxmin,uxmin1,1,real_type,MPI_MIN,MPI_COMM_WORLD,code)
+    call MPI_ALLREDUCE(uxmax,uxmax1,1,real_type,MPI_MAX,MPI_COMM_WORLD,code)
+    call MPI_ALLREDUCE(uxmin,uxmin1,1,real_type,MPI_MIN,MPI_COMM_WORLD,code)
 
-  if (u1.eq.0) cx=(half*(uxmax1+uxmin1))*gdt(itr)*udx
-  if (u1.eq.1) cx=uxmax1*gdt(itr)*udx
-  if (u1.eq.2) cx=u2*gdt(itr)*udx    !works better
+    if (u1.eq.0) cx=(half*(uxmax1+uxmin1))*gdt(itr)*udx
+    if (u1.eq.1) cx=uxmax1*gdt(itr)*udx
+    if (u1.eq.2) cx=u2*gdt(itr)*udx    !works better
 
-  do k=1,xsize(3)
-     do j=1,xsize(2)
-        bxxn(j,k)=ux(nx,j,k)-cx*(ux(nx,j,k)-ux(nx-1,j,k))
-        bxyn(j,k)=uy(nx,j,k)-cx*(uy(nx,j,k)-uy(nx-1,j,k))
-        bxzn(j,k)=uz(nx,j,k)-cx*(uz(nx,j,k)-uz(nx-1,j,k))
-     enddo
-  enddo
+    do k=1,xsize(3)
+       do j=1,xsize(2)
+          bxxn(j,k)=ux(nx,j,k)-cx*(ux(nx,j,k)-ux(nx-1,j,k))
+          bxyn(j,k)=uy(nx,j,k)-cx*(uy(nx,j,k)-uy(nx-1,j,k))
+          bxzn(j,k)=uz(nx,j,k)-cx*(uz(nx,j,k)-uz(nx-1,j,k))
+       enddo
+    enddo
 
-  if (iscalar==1) then
-     if (u2.eq.0.) cx=(half*(uxmax1+uxmin1))*gdt(itr)*udx
-     if (u2.eq.1.) cx=uxmax1*gdt(itr)*udx
-     if (u2.eq.2.) cx=u2*gdt(itr)*udx    !works better
-     do k=1,xsize(3)
-        do j=1,xsize(2)
-           phi(nx,j,k,:)=phi(nx,j,k,:)-cx*(phi(nx,j,k,:)-phi(nx-1,j,k,:))
-        enddo
-     enddo
-  endif
+    if (iscalar==1) then
+       if (u2.eq.0.) cx=(half*(uxmax1+uxmin1))*gdt(itr)*udx
+       if (u2.eq.1.) cx=uxmax1*gdt(itr)*udx
+       if (u2.eq.2.) cx=u2*gdt(itr)*udx    !works better
+       do k=1,xsize(3)
+          do j=1,xsize(2)
+             phi(nx,j,k,:)=phi(nx,j,k,:)-cx*(phi(nx,j,k,:)-phi(nx-1,j,k,:))
+          enddo
+       enddo
+    endif
 
-  if (nrank==0) write(*,*) "Outflow velocity ux nx=n min max=",real(uxmin1,4),real(uxmax1,4)
+    if (nrank==0) write(*,*) "Outflow velocity ux nx=n min max=",real(uxmin1,4),real(uxmax1,4)
 
-  return
-end subroutine outflow
-!********************************************************************
-subroutine init_cyl (ux1,uy1,uz1,ep1,phi1,dux1,duy1,duz1,phis1,phiss1)
+    return
+  end subroutine outflow
+  !********************************************************************
+  subroutine init_cyl (ux1,uy1,uz1,ep1,phi1,dux1,duy1,duz1,phis1,phiss1)
 
-  USE decomp_2d
-  USE decomp_2d_io
-  USE variables
-  USE param
-  USE MPI
+    USE decomp_2d
+    USE decomp_2d_io
+    USE variables
+    USE param
+    USE MPI
 
-  implicit none
+    implicit none
 
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux1,uy1,uz1,ep1
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi1,phis1,phiss1
-  real(mytype),dimension(xsize(1),xsize(2),xsize(3),ntime) :: dux1,duy1,duz1
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3)) :: ux1,uy1,uz1,ep1
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3),numscalar) :: phi1,phis1,phiss1
+    real(mytype),dimension(xsize(1),xsize(2),xsize(3),ntime) :: dux1,duy1,duz1
 
-  real(mytype) :: y,r,um,r3,x,z,h,ct
-  real(mytype) :: cx0,cy0,cz0,hg,lg
-  integer :: k,j,i,ijk,fh,ierror,ii,is,code
-  integer (kind=MPI_OFFSET_KIND) :: disp
+    real(mytype) :: y,r,um,r3,x,z,h,ct
+    real(mytype) :: cx0,cy0,cz0,hg,lg
+    integer :: k,j,i,ijk,fh,ierror,ii,is,code
+    integer (kind=MPI_OFFSET_KIND) :: disp
 
-  integer, dimension (:), allocatable :: seed
+    integer, dimension (:), allocatable :: seed
 
-  if (iscalar==1) then
+    if (iscalar==1) then
 
-     phi1 = one !change as much as you want
+       phi1 = one !change as much as you want
 
-     !do not delete this
-     phis1=phi1
-     phiss1=phis1
+       !do not delete this
+       phis1=phi1
+       phiss1=phis1
 
-  endif
+    endif
 
-  ux1=zero; uy1=zero; uz1=zero
+    ux1=zero; uy1=zero; uz1=zero
 
-  if (iin.ne.0) then
-     call system_clock(count=code)
-     if (iin.eq.2) code=0
-     call random_seed(size = ii)
-     call random_seed(put = code+63946*nrank*(/ (i - 1, i = 1, ii) /))
+    if (iin.ne.0) then
+       call system_clock(count=code)
+       if (iin.eq.2) code=0
+       call random_seed(size = ii)
+       call random_seed(put = code+63946*nrank*(/ (i - 1, i = 1, ii) /))
 
-     call random_number(ux1)
-     call random_number(uy1)
-     call random_number(uz1)
+       call random_number(ux1)
+       call random_number(uy1)
+       call random_number(uz1)
 
 
-     do k=1,xsize(3)
-        do j=1,xsize(2)
-           do i=1,xsize(1)
-              ux1(i,j,k)=init_noise*(ux1(i,j,k)-0.5)
-              uy1(i,j,k)=init_noise*(uy1(i,j,k)-0.5)
-              uz1(i,j,k)=init_noise*(uz1(i,j,k)-0.5)
-           enddo
-        enddo
-     enddo
+       do k=1,xsize(3)
+          do j=1,xsize(2)
+             do i=1,xsize(1)
+                ux1(i,j,k)=init_noise*(ux1(i,j,k)-0.5)
+                uy1(i,j,k)=init_noise*(uy1(i,j,k)-0.5)
+                uz1(i,j,k)=init_noise*(uz1(i,j,k)-0.5)
+             enddo
+          enddo
+       enddo
 
-     !modulation of the random noise
-     do k=1,xsize(3)
-        do j=1,xsize(2)
-           if (istret.eq.0) y=(j+xstart(2)-1-1)*dy-yly/2.
-           if (istret.ne.0) y=yp(j+xstart(2)-1)-yly/2.
-           um=exp(-zptwo*y*y)
-           do i=1,xsize(1)
-              ux1(i,j,k)=um*ux1(i,j,k)
-              uy1(i,j,k)=um*uy1(i,j,k)
-              uz1(i,j,k)=um*uz1(i,j,k)
-           enddo
-        enddo
-     enddo
-  endif
+       !modulation of the random noise
+       do k=1,xsize(3)
+          do j=1,xsize(2)
+             if (istret.eq.0) y=(j+xstart(2)-1-1)*dy-yly/2.
+             if (istret.ne.0) y=yp(j+xstart(2)-1)-yly/2.
+             um=exp(-zptwo*y*y)
+             do i=1,xsize(1)
+                ux1(i,j,k)=um*ux1(i,j,k)
+                uy1(i,j,k)=um*uy1(i,j,k)
+                uz1(i,j,k)=um*uz1(i,j,k)
+             enddo
+          enddo
+       enddo
+    endif
 
-  !INIT FOR G AND U=MEAN FLOW + NOISE
-  do k=1,xsize(3)
-     do j=1,xsize(2)
-        do i=1,xsize(1)
-           ux1(i,j,k)=ux1(i,j,k)+one
-           uy1(i,j,k)=uy1(i,j,k)
-           uz1(i,j,k)=uz1(i,j,k)
-           dux1(i,j,k,1)=ux1(i,j,k)
-           duy1(i,j,k,1)=uy1(i,j,k)
-           duz1(i,j,k,1)=uz1(i,j,k)
-           dux1(i,j,k,2)=dux1(i,j,k,1)
-           duy1(i,j,k,2)=duy1(i,j,k,1)
-           duz1(i,j,k,2)=duz1(i,j,k,1)
-        enddo
-     enddo
-  enddo
+    !INIT FOR G AND U=MEAN FLOW + NOISE
+    do k=1,xsize(3)
+       do j=1,xsize(2)
+          do i=1,xsize(1)
+             ux1(i,j,k)=ux1(i,j,k)+one
+             uy1(i,j,k)=uy1(i,j,k)
+             uz1(i,j,k)=uz1(i,j,k)
+             dux1(i,j,k,1)=ux1(i,j,k)
+             duy1(i,j,k,1)=uy1(i,j,k)
+             duz1(i,j,k,1)=uz1(i,j,k)
+             dux1(i,j,k,2)=dux1(i,j,k,1)
+             duy1(i,j,k,2)=duy1(i,j,k,1)
+             duz1(i,j,k,2)=duz1(i,j,k,1)
+          enddo
+       enddo
+    enddo
 
 #ifdef DEBG
-  if (nrank .eq. 0) print *,'# init end ok'
+    if (nrank .eq. 0) print *,'# init end ok'
 #endif
 
-  return
-end subroutine init_cyl
-!********************************************************************
+    return
+  end subroutine init_cyl
+  !********************************************************************
 
   !############################################################################
   subroutine init_post(ep1)
@@ -360,7 +360,7 @@ end subroutine init_cyl
     character(len=30) :: filename
 
     if (itime.ge.ioutput) then
-    
+
        !x-derivatives
        call derx (ta1,ux1,di1,sx,ffx,fsx,fwx,xsize(1),xsize(2),xsize(3),0)
        call derx (tb1,uy1,di1,sx,ffxp,fsxp,fwxp,xsize(1),xsize(2),xsize(3),1)
@@ -426,14 +426,14 @@ end subroutine init_cyl
        !############################################################################
        !PRESSURE       
        !NOT READY
-       
- endif   
-    
+
+    endif
+
     if (itime.ge.initstat) then
        !umean=ux1
        call fine_to_coarseS(1,ux1,tmean)
        umean(:,:,:)=umean(:,:,:)+tmean(:,:,:)
-       
+
        !vmean=uy1
        call fine_to_coarseS(1,uy1,tmean)
        vmean(:,:,:)=vmean(:,:,:)+tmean(:,:,:)
