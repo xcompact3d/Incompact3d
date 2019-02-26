c23edc6df4222cd49506f508fdaffd1060adce0b
Paul Bartholomew <paul.bartholomew08@imperial.ac.uk>
2019-02-26 14:48:31 +0000

    Adding visu subroutine to incompact3d.f90
    Currently only writes velocity and vorticity

--- src/incompact3d.f90
+++ src/incompact3d.f90
@@ -173,6 +173,8 @@ PROGRAM incompact3d
      endif
 
      call simu_stats(3)
+
+     CALL visu(ux1, uy1, uz1, itime)
      !!----------------------------------------------------------------------------
      !! End post-processing / IO
      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -249,3 +251,96 @@ SUBROUTINE solve_poisson(pp3, ux1, uy1, uz1, ep1)
   CALL poisson(pp3)
 
 END SUBROUTINE solve_poisson
+
+SUBROUTINE visu(ux1, uy1, uz1, itime)
+
+  USE decomp_2d, ONLY : mytype, xsize, ysize, zsize
+  USE decomp_2d, ONLY : fine_to_coarseV, transpose_x_to_y, transpose_y_to_z, &
+       transpose_z_to_y, transpose_y_to_x
+  USE decomp_2d_io, ONLY : decomp_2d_write_one
+  USE param, ONLY : ivisu, ioutput
+  USE variables, ONLY : sx, ffx, fsx, fwx, ffxp, fsxp, fwxp
+  USE variables, ONLY : sy, ffy, fsy, fwy, ffyp, fsyp, fwyp, ppy
+  USE variables, ONLY : sz, ffz, fsz, fwz, ffzp, fszp, fwzp
+  USE variables, ONLY : derx, dery, derz
+  USE var, ONLY : uvisu
+  USE var, ONLY : ta1,tb1,tc1,td1,te1,tf1,tg1,th1,ti1,di1
+  USE var, ONLY : ta2,tb2,tc2,td2,te2,tf2,di2,ta3,tb3,tc3,td3,te3,tf3,di3
+
+  IMPLICIT NONE
+
+  CHARACTER(len=30) :: filename
+  
+  !! Inputs
+  REAL(mytype), DIMENSION(xsize(1), xsize(2), xsize(3)), INTENT(IN) :: ux1, uy1, uz1
+  INTEGER, INTENT(IN) :: itime
+
+  IF ((ivisu.NE.0).AND.(MOD(itime, ioutput).EQ.0)) THEN
+     !! Write velocity
+     uvisu=0.
+     call fine_to_coarseV(1,ux1,uvisu)
+990  format('ux',I3.3)
+     write(filename, 990) itime/ioutput
+     call decomp_2d_write_one(1,uvisu,filename,2)
+     
+     uvisu=0.
+     call fine_to_coarseV(1,uy1,uvisu)
+991  format('uy',I3.3)
+     write(filename, 991) itime/ioutput
+     call decomp_2d_write_one(1,uvisu,filename,2)
+     
+     uvisu=0.
+     call fine_to_coarseV(1,uz1,uvisu)
+992  format('uz',I3.3)
+     write(filename, 992) itime/ioutput
+     call decomp_2d_write_one(1,uvisu,filename,2)
+
+     !! Calculate and write vorticity
+
+     !x-derivatives
+     call derx (ta1,ux1,di1,sx,ffx,fsx,fwx,xsize(1),xsize(2),xsize(3),0)
+     call derx (tb1,uy1,di1,sx,ffxp,fsxp,fwxp,xsize(1),xsize(2),xsize(3),1)
+     call derx (tc1,uz1,di1,sx,ffxp,fsxp,fwxp,xsize(1),xsize(2),xsize(3),1)
+     
+     !y-derivatives
+     call transpose_x_to_y(ux1,td2)
+     call transpose_x_to_y(uy1,te2)
+     call transpose_x_to_y(uz1,tf2)
+     call dery (ta2,td2,di2,sy,ffyp,fsyp,fwyp,ppy,ysize(1),ysize(2),ysize(3),1)
+     call dery (tb2,te2,di2,sy,ffy,fsy,fwy,ppy,ysize(1),ysize(2),ysize(3),0)
+     call dery (tc2,tf2,di2,sy,ffyp,fsyp,fwyp,ppy,ysize(1),ysize(2),ysize(3),1)
+     
+     !!z-derivatives
+     call transpose_y_to_z(td2,td3)
+     call transpose_y_to_z(te2,te3)
+     call transpose_y_to_z(tf2,tf3)
+     call derz (ta3,td3,di3,sz,ffzp,fszp,fwzp,zsize(1),zsize(2),zsize(3),1)
+     call derz (tb3,te3,di3,sz,ffzp,fszp,fwzp,zsize(1),zsize(2),zsize(3),1)
+     call derz (tc3,tf3,di3,sz,ffz,fsz,fwz,zsize(1),zsize(2),zsize(3),0)
+     
+     !!all back to x-pencils
+     call transpose_z_to_y(ta3,td2)
+     call transpose_z_to_y(tb3,te2)
+     call transpose_z_to_y(tc3,tf2)
+     call transpose_y_to_x(td2,tg1)
+     call transpose_y_to_x(te2,th1)
+     call transpose_y_to_x(tf2,ti1)
+     call transpose_y_to_x(ta2,td1)
+     call transpose_y_to_x(tb2,te1)
+     call transpose_y_to_x(tc2,tf1)
+     
+     !du/dx=ta1 du/dy=td1 and du/dz=tg1
+     !dv/dx=tb1 dv/dy=te1 and dv/dz=th1
+     !dw/dx=tc1 dw/dy=tf1 and dw/dz=ti1
+     
+     di1(:,:,:)=sqrt((tf1(:,:,:)-th1(:,:,:))**2+(tg1(:,:,:)-tc1(:,:,:))**2+&
+          (tb1(:,:,:)-td1(:,:,:))**2)
+     uvisu=0.
+     call fine_to_coarseV(1,di1,uvisu)
+993  format('vort',I3.3)
+     write(filename, 993) itime/ioutput
+     call decomp_2d_write_one(1,uvisu,filename,2)
+
+     !! TODO Write pressure
+  ENDIF
+END SUBROUTINE visu
