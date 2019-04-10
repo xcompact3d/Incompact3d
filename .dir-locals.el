1260b50e891f965eeae28d0b78c686595d06b92c
Paul Bartholomew <paul.bartholomew08@imperial.ac.uk>
2019-04-10 10:01:37 +0100

    Fixing .dir-locals.el
    Now correctly sets flycheck-gfortran-include-path and sets the
    language standard to -std=f2003

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ((nil . ((eval . (setq flycheck-gfortran-include-path
-			(file-name-directory
-			 (let ((d (dir-locals-find-file ".")))
-			   (if (stringp d)
-			       d
-			     (car d)))))))))
+											 (list (file-name-directory
+															(let ((d (dir-locals-find-file ".")))
+																(if (stringp d)
+																		d
+																	(car d)))))))
+				 (eval . (setq flycheck-gfortran-language-standard "f2003")))))
