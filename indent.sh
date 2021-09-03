c7a4c231e5070f9305f74309bf746bc8ef969602
cflag <cedric.flageul@univ-poitiers.fr>
2021-09-03 15:23:36 +0200

    Update indent.sh to have homogeneous statements

--- indent.sh
+++ indent.sh
@@ -24,3 +24,20 @@ do
 	echo "Indenting ${f}"
 	emacs -batch ${f} --eval '(indent-region (point-min) (point-max) nil)' -f save-buffer 2> /dev/null
 done
+
+# Make sure
+# no .eq., use ==
+# no .ne., use /=
+# no .ge., use >=
+# no .le., use <=
+# no .gt., use >
+# no .le., use <
+for f in decomp2d/*f90 decomp2d/*.inc src/*f90
+do
+	sed -i 's!\.eq\.! == !g' $f
+	sed -i 's!\.ne\.! \/= !g' $f
+	sed -i 's!\.ge\.! >= !g' $f
+	sed -i 's!\.le\.! <= !g' $f
+	sed -i 's!\.gt\.! > !g' $f
+	sed -i 's!\.lt\.! < !g' $f
+done
