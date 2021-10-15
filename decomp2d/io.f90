e9c7839819e8e2705539605d8f44d47258fae31b
Stefano Rolfo <stefano.rolfo@stfc.ac.uk>
2021-10-16 00:26:03 +0100

    Enforce message on screen only for master node for ADIOS2 variable list

--- decomp2d/io.f90
+++ decomp2d/io.f90
@@ -875,7 +875,7 @@ contains
     call adios2_inquire_variable(var_handle, io, varname, ierror)
     if (.not.var_handle % valid) then
        !! New variable
-       print *, "Registering variable for IO: ", varname
+       if(nrank==0) print *, "Registering variable for IO: ", varname
    
        ! Need to set the ADIOS2 data type
        if (mytype_single.eq.kind(0.0d0)) then
