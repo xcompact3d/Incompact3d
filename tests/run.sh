c3a4a46b0fdf79763b4567e8c75cb786fb8d4926
cflag <cedric.flageul@univ-poitiers.fr>
2021-10-24 11:32:26 +0200

    Update unit tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-./derive/test_x_00
-./derive/test_y_00
-./derive/test_y_00_str
-./derive/test_z_00
+cd ./derive
+./test_x_00
+./test_y_00
+./test_y_00_str
+./test_z_00
+cd ../
