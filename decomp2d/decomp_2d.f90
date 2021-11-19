74caf6a283b5befc2d6f6d052df58938094cbbef
slaizet <45965875+slaizet@users.noreply.github.com>
2021-11-19 13:13:30 +0000

    update best_2d_grid

--- decomp2d/decomp_2d.f90
+++ decomp2d/decomp_2d.f90
@@ -1589,105 +1589,32 @@ contains
   ! Auto-tuning algorithm to select the best 2D processor grid
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   subroutine best_2d_grid(iproc, best_p_row, best_p_col)
-
     implicit none
 
     integer, intent(IN) :: iproc
     integer, intent(OUT) :: best_p_row, best_p_col
 
     integer, allocatable, dimension(:) :: factors
-    double precision :: t1, t2, best_time
-    integer :: nfact, i, row, col, ierror, errorcode
-
-    real(mytype), allocatable, dimension(:,:,:) :: u1, u2, u3
-
-    TYPE(DECOMP_INFO) :: decomp
+    integer :: nfact, i, row, col, i_best
 
     if (nrank==0) write(*,*) 'In auto-tuning mode......'
-
-    best_time = huge(t1)
-    best_p_row = -1
-    best_p_col = -1
 
     i = int(sqrt(real(iproc))) + 10  ! enough space to save all factors 
     allocate(factors(i))
     call findfactor(iproc, factors, nfact)
     if (nrank==0) write(*,*) 'factors: ', (factors(i), i=1,nfact)
 
-    do i=1, nfact
+    i_best=nfact/2+1
+    col=factors(i_best)
 
-       row = factors(i)
-       col = iproc / row
-
-       ! enforce the limitation of 2D decomposition
-       if (min(nx_global,ny_global)>=row .and. &
-            min(ny_global,nz_global)>=col) then
-
-          ! 2D Catersian topology
-          dims(1) = row
-          dims(2) = col
-          periodic(1) = .false.
-          periodic(2) = .false.
-          call MPI_CART_CREATE(MPI_COMM_WORLD,2,dims,periodic, &
-               .false.,DECOMP_2D_COMM_CART_X, ierror)
-          call MPI_CART_COORDS(DECOMP_2D_COMM_CART_X,nrank,2,coord,ierror)
-
-          ! communicators defining sub-groups for ALLTOALL(V)
-          call MPI_CART_SUB(DECOMP_2D_COMM_CART_X,(/.true.,.false./), &
-               DECOMP_2D_COMM_COL,ierror)
-          call MPI_CART_SUB(DECOMP_2D_COMM_CART_X,(/.false.,.true./), &
-               DECOMP_2D_COMM_ROW,ierror)
-
-          ! generate 2D decomposition information for this row*col
-          call decomp_info_init(nx_global,ny_global,nz_global,decomp)
-
-          ! arrays for X,Y and Z-pencils
-          allocate(u1(decomp%xsz(1),decomp%xsz(2),decomp%xsz(3)))
-          allocate(u2(decomp%ysz(1),decomp%ysz(2),decomp%ysz(3)))
-          allocate(u3(decomp%zsz(1),decomp%zsz(2),decomp%zsz(3)))
-
-          ! timing the transposition routines
-          t1 = MPI_WTIME()
-          call transpose_x_to_y(u1,u2,decomp)
-          call transpose_y_to_z(u2,u3,decomp)
-          call transpose_z_to_y(u3,u2,decomp)
-          call transpose_y_to_x(u2,u1,decomp)
-          t2 = MPI_WTIME() - t1
-
-          deallocate(u1,u2,u3)
-          call decomp_info_finalize(decomp)
-
-          call MPI_ALLREDUCE(t2,t1,1,MPI_DOUBLE_PRECISION,MPI_SUM, &
-               MPI_COMM_WORLD,ierror)
-          t1 = t1 / dble(nproc)
-
-          if (nrank==0) then
-             write(*,*) 'processor grid', row, ' by ', col, ' time=', t1
-          end if
-
-          if (best_time > t1) then
-             best_time = t1
-             best_p_row = row
-             best_p_col = col
-          end if
-
-       end if
-
-    end do ! loop through processer grid
-
+    best_p_col = col
+    best_p_row=iproc/col
+    if (nrank==0) print *,'p_row x p_col', best_p_row, best_p_col
+    if ((best_p_col==1).and.(nrank==0)) then
+       print *,'WARNING: current 2D DECOMP set-up might not work'
+    endif
+    
     deallocate(factors)
-
-    if (best_p_row/=-1) then
-       if (nrank==0) then
-          write(*,*) 'the best processor grid is probably ', &
-               best_p_row, ' by ', best_p_col
-       end if
-    else
-       errorcode = 9
-       call decomp_2d_abort(errorcode, &
-            'The processor-grid auto-tuning code failed. ' // &
-            'The number of processes requested is probably too large.')
-    end if
 
     return
   end subroutine best_2d_grid
