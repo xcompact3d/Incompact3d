d8263b980f3778db6a4b3b96be82cb8cc5d808ab
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2022-01-11 12:53:15 +0000

    Can now pip install decomp2d module
    Note that the user needs to pass an MPI Fortran compiler, this is
    handled by the Makefile which will also uninstall the module

--- decomp2d/python/setup.py
+++ decomp2d/python/setup.py
@@ -7,15 +7,27 @@ REQUIREMENTS=[
     "mpi4py"
 ]
 
+X3DDIR="/Users/paulbartholomew/src/fortran/Xcompact3d"
+D2DDIR=os.path.join(X3DDIR, "decomp2d")
+
 d2dext = Extension(
     name = "decomp2d",
     sources = [
         "2decomp4py.f90"
     ],
     include_dirs = [
-        ".",
-        "../",
-        "../../"
+        os.path.join(D2DDIR, "python"),
+        D2DDIR,
+        X3DDIR
+    ],
+    define_macros = [
+        ("DOUBLE_PREC", None)
+    ],
+    extra_objects = [
+      os.path.join(D2DDIR, "decomp_2d.o")  
+    ],
+    extra_f90_compile_args = [
+        "-cpp"
     ]
 )
 
