b7e346eebb020ec4b0b8262b8e19e6def30fdfab
Paul <ptb0890@gmail.com>
2022-05-13 10:03:14 +0100

    Delete unused variable

--- decomp2d/decomp_2d.f90
+++ decomp2d/decomp_2d.f90
@@ -1206,7 +1206,6 @@ contains
     !         et le calcul plantait dans MPI_ALLTOALLV
     !       * pas de plantage en O2
     
-    character(len=100) :: tmp_char
     if (nrank==0) then
      open(newunit=i,file='temp.dat')
      write(i) decomp%x1dist,decomp%y1dist,decomp%y2dist,decomp%z2dist, &
