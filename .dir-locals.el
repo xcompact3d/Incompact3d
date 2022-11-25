40960f5c8894558cb06f3503d582b254f3ed14df
Paul <ptb0890@gmail.com>
2022-11-25 11:57:38 +0000

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,4 +5,5 @@
   (fortan-comment-indent-style . 'relative)
   (flycheck-gfortran-language-standard . "f2003")
   (flycheck-gfortran-args . ("-fcray-pointer" "-cpp"))
-  (flycheck-fortran-gfortran-executable . "mpif90")))
+  (flycheck-fortran-gfortran-executable . "mpif90.openmpi")
+  (flycheck-gfortran-include-path . ("/home/paul/src/Xcompact3d/Incompact3d"))))
