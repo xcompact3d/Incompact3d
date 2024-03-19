b354978fd09ff646a64f58847b26fc94593de1f8
cflag <cedric.flageul@univ-poitiers.fr>
2024-03-19 08:55:58 +0100

    Import RELEASE and DEBUG flags from the Makefile for the GNU compiler

--- cmake/compilers/X3D_flags_gnu.cmake
+++ cmake/compilers/X3D_flags_gnu.cmake
@@ -8,8 +8,10 @@ if (CMAKE_Fortran_COMPILER_VERSION GREATER_EQUAL "10")
 else (CMAKE_Fortran_COMPILER_VERSION GREATER_EQUAL "10")
   set(X3D_GNU10 FALSE)
 endif (CMAKE_Fortran_COMPILER_VERSION GREATER_EQUAL "10")
-set(X3D_FFLAGS_RELEASE "-O3 -funroll-loops -floop-optimize -march=native")
-set(X3D_FFLAGS_DEBUG   "-DDEBUG -g3 -Og -ffpe-trap=invalid,zero -fcheck=all -fimplicit-none")
+# Flags extracted from the Makefile
+set(X3D_FFLAGS_RELEASE "-O3 -funroll-loops -floop-optimize -g") #"-O3 -funroll-loops -floop-optimize -march=native")
+set(X3D_FFLAGS_DEBUG "-g3 -Og -ffpe-trap=invalid,zero -fcheck=bounds -fimplicit-none") #"-DDEBUG -g3 -Og -ffpe-trap=invalid,zero -fcheck=all -fimplicit-none")
+# Dev flag below is new and not working yet
 if (FIND_MPICH AND X3D_GNU10)
   set(X3D_FFLAGS_DEV     "${X3D_FFLAGS_DEBUG} -Wall -Wno-unused-function -Wno-integer-division")
 else()
