5bc9c27774a0d43ba6ba441e7bdbb87c61af2ff3
slaizet <s.laizet@imperial.ac.uk>
2024-04-17 15:53:35 +0100

    removal of constants.f90 file

--- src/module_param.f90
+++ src/module_param.f90
@@ -622,6 +622,23 @@ module ibm_param
 end module ibm_param
 !############################################################################
 !############################################################################
+module constants
+
+    use decomp_2d_constants, only: mytype
+    use param, only: onehundredeighty
+
+    ! Mathematical constants
+    real(mytype), parameter :: pi = 3.14159265358979323846_mytype
+    real(mytype), parameter :: conrad = pi / onehundredeighty
+    real(mytype), parameter :: condeg = onehundredeighty / pi
+
+    ! Definition of maximum size of arrays
+    integer, parameter :: MaxNAirfoils = 80 ! Maximum number of airfoils to be read
+    integer, parameter :: MaxReVals = 10    ! Maximum number of tables (one per Reynolds number) that will be read
+    integer, parameter :: MaxAOAVals = 1000 ! Maximum number of angles of attack in each polar that will be read
+
+end module constants
+!############################################################################
 module dbg_schemes
 
   use decomp_2d
