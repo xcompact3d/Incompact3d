b92628b5f2cb4abe44b3355d2aee46bc5a353450
s2006749 <s2006749@ed.ac.uk>
2024-06-17 13:41:36 +0100

    even more debug comments

--- src/ellip_utils.f90
+++ src/ellip_utils.f90
@@ -221,6 +221,8 @@ contains
       if (radius /= radius) then
         write(*,*) "Got an error in grid check!"
         write(*,*) "Radius = ", radius, "point = ", point
+        write(*,*) "Rotated point = ", rotated_point
+        write(*,*) "Scaled Point = ", scaled_point
       endif
 
    end subroutine    
