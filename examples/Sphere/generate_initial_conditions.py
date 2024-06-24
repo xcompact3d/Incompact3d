57c7c4fba19e58c37c8b3eaed2bdcbf812d2c156
Jacques Xing <jxing1@kolmogorov.ae.ic.ac.uk>
2024-06-24 21:11:33 +0100

    Fix examples/Sphere/generate_initial_conditions.py

--- examples/Sphere/generate_initial_conditions.py
+++ examples/Sphere/generate_initial_conditions.py
@@ -14,12 +14,12 @@ epsi = x3d.init_epsi(prm, dask=True)
 # Read the STL file
 for key in epsi.keys():
     print(key)
-    epsi[key] = epsi[key].geo.from_stl("sphere.stl",
+    epsi[key] = epsi[key].geo.from_stl(filename="sphere.stl",
                                        origin=dict(x=2.0,y=2.0,z=2.0), # locates the sphere centre at (2.5, 2.5, 2.5)
                                        scale=1.0/200.0, # Sphere has a diameter of 200, so rescale to a diameter of 1
                                        # rotate=dict(axis=[0,0,1],theta=math.radians(270)), # rotate the geometry
                                        user_tol=1e-3)
-dataset = x3d.gene_epsi_3D(epsi, prm)
+dataset = x3d.gene_epsi_3d(epsi, prm)
 
 if prm.iibm >= 2:
     prm.nobjmax = dataset.obj.size
