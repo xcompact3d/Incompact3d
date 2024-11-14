201a3ac221c9645944605bfb097708546e1597d2
Stefano  Rolfo <35581400+rfj82982@users.noreply.github.com>
2024-11-14 16:50:24 +0000

    Add NREL Wind-Turbine to examples and tests

--- examples/cmake/X3D_ADD_EXAMPLE.cmake
+++ examples/cmake/X3D_ADD_EXAMPLE.cmake
@@ -1,9 +1,9 @@
-macro(AddExample example_dir case list_files)
-  install(DIRECTORY DESTINATION ${example_dir}/${case})
+macro(AddExample root_dir case list_files)
+  install(DIRECTORY DESTINATION ${root_dir}/${case})
   set(local_list "")
   list(APPEND local_list ${list_files})
   foreach(ff IN LISTS local_list)
-    install(FILES ${ff} DESTINATION ${example_dir}/${case})
+    install(FILES ${ff} DESTINATION ${root_dir}/${case})
   endforeach()
 endmacro()
 
