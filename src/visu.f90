6845f72cf2866f1caef81ffc52b469f840b3c9d7
s2006749 <s2006749@ed.ac.uk>
2025-03-04 11:04:34 +0000

    closest to official branch without printing div_visu

--- src/visu.f90
+++ src/visu.f90
@@ -264,25 +264,25 @@ contains
     ! Write pressure
     call write_field(ta1, ".", "pp", num, .true., flush=.true.)
 
-     ! Interpolate div_visu_var
-    !WORK Z-PENCILS
-    call interzpv(ppi3,div_visu_var(:,:,:,1),dip3,sz,cifip6z,cisip6z,ciwip6z,cifz6,cisz6,ciwz6,&
-    (ph3%zen(1)-ph3%zst(1)+1),(ph3%zen(2)-ph3%zst(2)+1),nzmsize,zsize(3),1)
-    !WORK Y-PENCILS
-    call transpose_z_to_y(ppi3,pp2,ph3) !nxm nym nz
-    call interypv(ppi2,pp2,dip2,sy,cifip6y,cisip6y,ciwip6y,cify6,cisy6,ciwy6,&
-            (ph3%yen(1)-ph3%yst(1)+1),nymsize,ysize(2),ysize(3),1)
-    !WORK X-PENCILS
-    call transpose_y_to_x(ppi2,pp1,ph2) !nxm ny nz
-    call interxpv(ta1,pp1,di1,sx,cifip6,cisip6,ciwip6,cifx6,cisx6,ciwx6,&
-            nxmsize,xsize(1),xsize(2),xsize(3),1)
+    !  ! Interpolate div_visu_var
+    ! !WORK Z-PENCILS
+    ! call interzpv(ppi3,div_visu_var(:,:,:,1),dip3,sz,cifip6z,cisip6z,ciwip6z,cifz6,cisz6,ciwz6,&
+    ! (ph3%zen(1)-ph3%zst(1)+1),(ph3%zen(2)-ph3%zst(2)+1),nzmsize,zsize(3),1)
+    ! !WORK Y-PENCILS
+    ! call transpose_z_to_y(ppi3,pp2,ph3) !nxm nym nz
+    ! call interypv(ppi2,pp2,dip2,sy,cifip6y,cisip6y,ciwip6y,cify6,cisy6,ciwy6,&
+    !         (ph3%yen(1)-ph3%yst(1)+1),nymsize,ysize(2),ysize(3),1)
+    ! !WORK X-PENCILS
+    ! call transpose_y_to_x(ppi2,pp1,ph2) !nxm ny nz
+    ! call interxpv(ta1,pp1,di1,sx,cifip6,cisip6,ciwip6,cifx6,cisx6,ciwx6,&
+    !         nxmsize,xsize(1),xsize(2),xsize(3),1)
 
 
-    ! Rescale div_visu_var
-    call rescale_pressure(ta1)
+    ! ! Rescale div_visu_var
+    ! call rescale_pressure(ta1)
 
-    ! Write pressure
-    call write_field(ta1, ".", "div_visu", num, .true., flush=.true.)
+    ! ! Write pressure
+    ! call write_field(ta1, ".", "div_visu", num, .true., flush=.true.)
 
     ! LMN - write density
     if (ilmn) call write_field(rho1(:,:,:,1), ".", "rho", num)
